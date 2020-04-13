6202b2d9a7867cd428d0d5114ce5cbab7f8a6ce4
IonesioJunior <ionesiojr@gmail.com>
2020-04-12 22:07:35 -0300

    Update routes.nodes,py - Finish  /nodes route - Finish /nodes/<id> route

--- app/main/routes/nodes.py
+++ app/main/routes/nodes.py
@@ -1,6 +1,6 @@
 from .. import http
 from ..events.socket_handler import SocketHandler
-
+from ..codes import MSG_FIELD
 from flask import Response
 import json
 
@@ -23,16 +23,24 @@ def connected_nodes():
 
 
 @http.route("/node/<id>")
-def node_infos(node_id):
-    worker = socket_handler.get(node_id)
+def node_infos(id):
+    worker = socket_handler.get(id)
     response = {}
+
     if worker:
         response["id"] = worker._id
         response["address"] = worker.address
         response["nodes"] = worker.connected_nodes
-        response["datasets"] = worker.datasets
-        response["models"] = worker.models
+        response["datasets"] = worker.hosted_datasets
+        response["models"] = worker.hosted_models
         response_body = json.dumps(response)
         return Response(response_body, status=200, mimetype="application/json")
     else:
-        return Response("", status=500, mimetype="application/json")
+        return Response(
+            {
+                MSG_FIELD.STATUS: MSG_FIELD.ERROR,
+                MSG_FIELD.CONTENT: "Worker ID not found!",
+            },
+            status=404,
+            mimetype="application/json",
+        )

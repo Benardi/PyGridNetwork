c807c7d32e3d886ec740901f42823d6e24a388f9
Ionésio Junior <ionesiojr@gmail.com>
2020-05-20 23:12:20 -0300

    Update dataset.py
    Add try/catch scope to avoid an unexpected crash.

--- app/main/routes/dataset.py
+++ app/main/routes/dataset.py
@@ -9,10 +9,13 @@ socket_handler = SocketHandler()
 
 @http.route("/datasets", methods=["GET"])
 def get_datasets():
-    response = {}
+    try:
+        response = {}
 
-    for node_id, node in socket_handler.nodes:
-        response[node_id] = node.hosted_datasets
+        for node_id, node in socket_handler.nodes:
+            response[node_id] = node.hosted_datasets
 
-    response_body = json.dumps(response)
-    return Response(response_body, status=200, mimetype="application/json")
+        response_body = json.dumps(response)
+        return Response(response_body, status=200, mimetype="application/json")
+    except Exception:
+        return Response({}, status=500, mimetype="application/json")

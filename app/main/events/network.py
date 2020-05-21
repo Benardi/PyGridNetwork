5983bebbd01917dad42cb865597874d149037842
Ionésio Junior <ionesiojr@gmail.com>
2020-05-20 23:07:04 -0300

    Update network.py
    Add try/catch scopes to avoid an unexpected crashes.

--- app/main/events/network.py
+++ app/main/events/network.py
@@ -8,23 +8,32 @@ socket_handler = SocketHandler()
 
 
 def update_node(message, socket):
-    worker = socket_handler.get(socket)
-    worker.update_node_infos(message)
+    try:
+        worker = socket_handler.get(socket)
+        worker.update_node_infos(message)
+    except Exception:
+        pass
 
 
 def register_node(message, socket):
-    time.sleep(1)
-    node_id = message[MSG_FIELD.NODE_ID]
-    worker = socket_handler.new_connection(node_id, socket)
-    t = threading.Thread(target=worker.monitor)
-    t.start()
-    return {"status": "success!"}
+    try:
+        time.sleep(1)
+        node_id = message[MSG_FIELD.NODE_ID]
+        worker = socket_handler.new_connection(node_id, socket)
+        t = threading.Thread(target=worker.monitor)
+        t.start()
+        return {"status": "success!"}
+    except Exception:
+        pass
 
 
 def forward(message, socket):
-    time.sleep(1)
-    dest = message[MSG_FIELD.DESTINATION]
-    worker = socket_handler.get(dest)
-    if worker:
-        content = message[MSG_FIELD.CONTENT]
-        worker.send(json.dumps(content))
+    try:
+        time.sleep(1)
+        dest = message[MSG_FIELD.DESTINATION]
+        worker = socket_handler.get(dest)
+        if worker:
+            content = message[MSG_FIELD.CONTENT]
+            worker.send(json.dumps(content))
+    except Exception:
+        pass

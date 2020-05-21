1e3037f09d4f1ae25a8e941151480359d7ad6b82
Ionésio Junior <ionesiojr@gmail.com>
2020-05-20 23:09:31 -0300

    Update nodes.py
    Add try/catch scope to avoid an unexpected crash.
    #13

--- app/main/routes/nodes.py
+++ app/main/routes/nodes.py
@@ -8,27 +8,30 @@ socket_handler = SocketHandler()
 
 @http.route("/node/<id>")
 def node_infos(id):
-    worker = socket_handler.get(id)
-    response = {}
+    try:
+        worker = socket_handler.get(id)
+        response = {}
 
-    if worker:
-        response["id"] = worker._id
-        response["status"] = worker.status
-        response["address"] = worker.address
-        response["nodes"] = worker.connected_nodes
-        response["datasets"] = worker.hosted_datasets
-        response["models"] = list(worker.hosted_models.keys())
-        response["cpu"] = worker.cpu_percent
-        response["memory"] = worker.mem_usage
+        if worker:
+            response["id"] = worker._id
+            response["status"] = worker.status
+            response["address"] = worker.address
+            response["nodes"] = worker.connected_nodes
+            response["datasets"] = worker.hosted_datasets
+            response["models"] = list(worker.hosted_models.keys())
+            response["cpu"] = worker.cpu_percent
+            response["memory"] = worker.mem_usage
 
-        response_body = json.dumps(response)
-        return Response(response_body, status=200, mimetype="application/json")
-    else:
-        return Response(
-            {
-                MSG_FIELD.STATUS: MSG_FIELD.ERROR,
-                MSG_FIELD.CONTENT: "Worker ID not found!",
-            },
-            status=404,
-            mimetype="application/json",
-        )
+            response_body = json.dumps(response)
+            return Response(response_body, status=200, mimetype="application/json")
+        else:
+            return Response(
+                {
+                    MSG_FIELD.STATUS: MSG_FIELD.ERROR,
+                    MSG_FIELD.CONTENT: "Worker ID not found!",
+                },
+                status=404,
+                mimetype="application/json",
+            )
+    except Exception:
+        return Response({}, status=500, mimetype="application/json")
